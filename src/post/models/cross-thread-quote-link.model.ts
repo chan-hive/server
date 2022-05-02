dc0d7bc38b1d7d224683229d85fa18663c15e2e0
Sophia <beyondthesophia@gmail.com>
2022-05-03 08:47:20 +0900

    refactor(post): rename data field of post content api to prevent conflict

--- src/post/models/cross-thread-quote-link.model.ts
+++ src/post/models/cross-thread-quote-link.model.ts
@@ -3,7 +3,7 @@ import { Field, Int, ObjectType } from "@nestjs/graphql";
 @ObjectType()
 export class CrossThreadQuoteLinkContent {
     @Field(() => Int, { nullable: true })
-    public postId!: number | null | undefined;
+    public targetPostId!: number | null | undefined;
 
     @Field(() => Boolean)
     public isOP!: boolean;
